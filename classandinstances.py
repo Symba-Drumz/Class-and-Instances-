db462b3e7481952d16da1c46cc09a560c212f620
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-05-16 07:21:09 +0300

    add code demonstrating class, instance methods, instance and initialization

--- classandinstances.py
+++ classandinstances.py
@@ -6,3 +6,15 @@ class Dog:
         return f"{self.name} says Woof!"
 my_dog = Dog("Rusty")
 print(my_dog.bark())     
+
+class Pipe:
+    def __init__(self, length, diameter, material):
+        self.length = length
+        self.diameter = diameter
+        self.material = material
+
+    def flow_water(self):
+        print("water is flowing through the pipe.")
+
+my_pipe = Pipe(10, 5, "PVC")
+my_pipe.flow_water()  # Output: water is flowing through the pipe    
